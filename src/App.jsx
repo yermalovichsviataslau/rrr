c81f46e5e4e8265994347638696f4710563e4696
Yermalovich Sviataslau <sviatosla.ermolovich@gmail.com>
2026-07-21 12:27:53 +0300

    feat: add them dark/light

--- src/App.jsx
+++ src/App.jsx
@@ -2,21 +2,42 @@ import "./App.css";
 import Profile from "./components/Profile";
 import ShopBasket from "./components/ShopBasket";
 import Todolist from "./components/Todolist";
+import ThemeToggle from "./components/ThemeToggle";
+import { ThemeContext, ThemeProvider } from "./context/ThemeContext";
+import { useContext, useEffect } from "react";
+
+const AppContent = () => {
+  const { theme } = useContext(ThemeContext);
+
+  useEffect(() => {
+    if (theme === 'dark') {
+      document.body.classList.add('dark');
+    } else {
+      document.body.classList.remove('dark');
+    }
+  }, [theme]);
+  return (
+    <>
+        <ThemeToggle />
+        <p>1. Стейт — объект</p>
+        <Profile />
+        <br />
+
+        <p>2. Стейт — массив</p>
+        <Todolist />
+        <br />
+
+        <p>3. Стейт — массив объектов</p>
+        <ShopBasket />
+    </>
+  );
+};
 
 function App() {
   return (
-    <>
-      <p>1. Стейт — объект</p>
-      <Profile />
-      <br />
-
-      <p>2. Стейт — массив</p>
-      <Todolist />
-      <br />
-
-      <p>3. Стейт — массив объектов</p>
-      <ShopBasket />
-    </>
+    <ThemeProvider>
+      <AppContent />
+    </ThemeProvider>
   );
 }
 
---
+++ src/context/ThemeContext.jsx
@@ -0,0 +1,22 @@
+import { createContext, useState } from "react";
+
+const ThemeContext = createContext();
+
+export const ThemeProvider = ({ children }) => {
+  const [theme, setTheme] = useState("light");
+
+  const toggleTheme = () => {
+    setTheme(theme === "light" ? "dark" : "light");
+  };
+
+  const value = {
+    theme,
+    toggleTheme,
+  };
+
+  return (
+    <ThemeContext.Provider value={value}>{children}</ThemeContext.Provider>
+  );
+};
+
+export { ThemeContext };
